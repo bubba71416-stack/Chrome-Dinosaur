e2593b236c7e8d140d3ed12c2d511dc0364f636a
bubba71416-stack <bubba71416@gmail.com>
2026-02-17 19:27:39 -0600

    Update dinosaur.py

--- dinosaur.py
+++ dinosaur.py
@@ -1,27 +1,21 @@
 import pygame
+from pygame.locals import *
+import sys
 
-dinocolour = 255,255,255
-DINOHEIGHT = 40
-DINOWIDTH = 20
+pygame.init()
+WIDTH, HEIGHT = 800, 400
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
 
-class Dinosaur:
-    def __init__(self, surfaceHeight):
-        self.x = 60
-        self.y = 0
-        self.yvelocity = 0
-        self.height = DINOHEIGHT
-        self.width = DINOWIDTH
-        self.surfaceHeight = surfaceHeight
-    def jump(self): #When adding classes into function, the first parameter must be the parameter
-        if(self.y == 0): #Only allow jumping if the dinosaur is on the ground to prevent mid air jumps.
-            self.yvelocity = 300
-    def update(self, deltaTime): #Updates the y position of the dinosaur each second
-        self.yvelocity += -500*deltaTime #Gravity
-        self.y += self.yvelocity * deltaTime
-        if self.y < 0: #if the dinosaur sinks into the ground, make velocity and y = 0
-            self.y = 0
-            self.yvelocity = 0
+dino = Dinosaur(HEIGHT)
 
-        
-    def draw(self,display):
-        pygame.draw.rect(display,dinocolour,[self.x,self.surfaceHeight-self.y-self.height,self.width,self.height])
+running = True
+while running:
+    deltaTime = clock.tick(60) / 1000
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+        if event.type == KEYDOWN:
+            if event.key == K_SPACE:
+                dino.jump()
